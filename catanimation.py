0111ffbcae6db09ae5218af2f6fc83f67af6ec08
Matthew <brownmk@hawaii.edu>
2015-09-24 02:10:03 -1000

    first commit

--- catanimation.py
+++ catanimation.py
@@ -1,9 +1,9 @@
 import pygame, sys
-from pygame.locals import *
+from pygame.locals import*
 
 pygame.init()
 
-FPS = 30 #frames per second setting
+FPS = 10 #frames per second setting
 fpsClock = pygame.time.Clock()
 
 #set up the window
@@ -11,7 +11,7 @@ DISPLAYSURF = pygame.display.set_mode((400,300),0,32)
 pygame.display.set_caption('Animation')
 
 WHITE = (255,255,255)
-catImg = pygame.image.load('cat.bmp')
+catImg = pygame.image.load('cat.png')
 catx = 10
 caty = 10
 direction = 'right'
